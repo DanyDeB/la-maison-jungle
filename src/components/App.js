a216e9fe766bdf7e137c959469dd912bfe948191
Dany DeB <de.brito.ln@gmail.com>
2022-10-03 16:39:58 +0200

    C 2.5
    Chapitre 2.5

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import logo from '../assets/logo.png'
 import Banner from './Banner';
 import Cart from './Cart';
 import ShoppingList from './ShoppingList';
+import handleSubmit from './handleSubmit';
 
 function App() {
     return (
@@ -11,6 +12,10 @@ function App() {
                 <img src={logo} alt='La maison jungle' />
                 <h1 className='lmj-title'>La maison jungle</h1>
             </Banner>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name='my_input' defaultValue='Tapez votre texte' />
+                <button type='submit'>Entrer</button>
+            </form>
             {/* <Cart /> */}
             <ShoppingList />
         </div>
--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -3,19 +3,19 @@ import '../styles/PlantItem.css'
 
 function PlantItem({ id, cover, name, water, light}) {
     return (
-        <li key={id} className='lmj-plant-item' onClick={handleClick(name)}>
+        <li key={id} className='lmj-plant-item' onClick={() => handleClick(name)}>
             <img src={cover} alt={`${name} cover`} className="lmj-plant-item-cover" />
             {name}
             <div>
-                <CareScale careType='water' scaleValue={water}></CareScale>
-                <CareScale careType='light' scaleValue={light}></CareScale>
+                <CareScale careType='water' scaleValue={water} />
+                <CareScale careType='light' scaleValue={light} />
             </div>
         </li>
     )
 }
 
-function handleClick() {
-    console.log('Clique recu avec succès')
+function handleClick(e) {
+    console.log('Clique reçu avec succès', e)
 }
 
 export default PlantItem
